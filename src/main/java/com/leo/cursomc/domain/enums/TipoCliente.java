31b3b3ec7e50a80ca8b52c13ccfc3bf425886bb1
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2020-04-21 22:39:55 -0300

    Salvando perfis de usuário na base de dados

--- src/main/java/com/leo/cursomc/domain/enums/TipoCliente.java
+++ src/main/java/com/leo/cursomc/domain/enums/TipoCliente.java
@@ -2,7 +2,8 @@ package com.leo.cursomc.domain.enums;
 
 public enum TipoCliente {
 
-	PESSOA_FISICA(1, "Pessoa Física"), PESSOA_JURIDICA(2, "Pessoa Jurídica");
+	PESSOA_FISICA(1, "Pessoa Física"), 
+	PESSOA_JURIDICA(2, "Pessoa Jurídica");
 
 	private int cod;
 	private String descricao;
