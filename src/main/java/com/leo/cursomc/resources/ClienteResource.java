845858a1743bffc1ec91ab4dae33dfb7788e01cf
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2020-04-25 16:33:24 -0300

    Enviando imagem via endpoint

--- src/main/java/com/leo/cursomc/resources/ClienteResource.java
+++ src/main/java/com/leo/cursomc/resources/ClienteResource.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.leo.cursomc.domain.Cliente;
@@ -83,5 +84,10 @@ public class ClienteResource {
 		Page<ClienteDTO> listaDto = lista.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(listaDto);
 	}
-
+	
+	@PostMapping("/picture")
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name = "file") MultipartFile file){
+		URI uri = service.uploadProfilePicture(file);		
+		return ResponseEntity.created(uri).build();
+	}
 }
